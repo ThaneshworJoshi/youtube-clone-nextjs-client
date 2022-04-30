a08e9956b11f60dc3c9cf74201c3a77d421698f1
thaneshwor10@gmail.com <thaneshwor10@gmail.com>
2022-04-30 14:03:57 +0545

    bootstrap project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Youtube clone</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: 'light' }}
+      >
+        <NotificationsProvider>
+          <Component {...pageProps} />;
+        </NotificationsProvider>
+      </MantineProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
